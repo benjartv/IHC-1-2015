be2e68e189d0574a4c9ef770b974d6233595e7bc
Ismael Vicencio <imvicencio@live.cl>
2015-04-21 10:30:39 -0300

    Add model sin relaciones

--- l-pink/app/models/tema.rb
+++ l-pink/app/models/tema.rb
@@ -1,2 +1,11 @@
 class Tema < ActiveRecord::Base
+
+	self.primary_key = :id
+
+	validates :acta_id, presence: true
+    validates :tema_nombre, presence: true
+    validates :tema_descripcion, presence: true
+    validates :tema_estado, presence: true
+    validates :tema_correlativo, presence: true
+
 end
